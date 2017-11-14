28558c09ce01fbbcd37436b2381065085c06f7f2
rebeccaSimmonds19 <rmsimmonds1919@gmail.com>
2017-11-14 16:16:33 +0000

    changed the session builder

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ class MyClass:
         self.make(args.servers)
 
     def make(self, servers):
-        sparkSession = SparkSession.builder.appName("winemap") \
+        sparkSession = SparkSession.builder \
             .getOrCreate()
         conn = psycopg2.connect("host="+servers+" port='5432' dbname='wineDb' user='username' password='password'")
         cur = conn.cursor()
