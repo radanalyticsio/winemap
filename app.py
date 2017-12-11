47eb9988b13d45d73dfa8b7fa866b16baff01de9
rebeccaSimmonds19 <rmsimmonds1919@gmail.com>
2017-12-11 14:45:57 -0600

    got rid of .config

--- app.py
+++ app.py
@@ -24,10 +24,6 @@ class WineMapGenerator:
 
     def make(self, server, user, dbname, password):
         spark_session = SparkSession.builder \
-                      .config("spark.driver.extraClassPath",
-                              "/opt/app-root/src/.ivy2/jars/org.postgresql_postgresql-42.1.4.jar")\
-            .config("spark.executor.extraClassPath" ,
-                    "/opt/app-root/src/.ivy2/jars/org.postgresql_postgresql-42.1.4.jar")\
             .getOrCreate()
 
         url = "jdbc:postgresql://" + server\
